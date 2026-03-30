e4be8bd3b8becd0b8e1e755a88fd378c968499c0
Jerem39244 <Jeremychacko12@icloud.com>
2026-03-30 18:34:09 -0400

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,6 @@
+def factorial(n):
+    if n==1:
+        return 0
+    else:
+        return n*factorial(n-1)
+    
---
+++ introduction.py
@@ -0,0 +1,4 @@
+def intro(name):
+    print(f"Hello I am {name}")
+a = input("What is your name?")
+intro(a)
